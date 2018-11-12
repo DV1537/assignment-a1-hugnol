4bcbee6bcd6bafc39a0e1d01232c37e4a5a08ab5
hugnol <hugonolte@hotmail.se>
2018-11-12 08:19:57 +0100

    First version assignment A1

---
+++ A1/A1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    int size = 1, numbers = 0, subscript = 0;
+    int *fileData = new int[size];
+    int *tempData = new int[size];
+
+    std::ifstream inputStream;
+    inputStream.open("number.txt");
+
+    while ((inputStream >> numbers) && !(std::cin.fail())) //If the instream reads a non integer value, the loop will exit.
+    {
+        if (subscript > size)
+        {
+            for (int i = 0; i < subscript; i++)
+            {
+                tempData[i] = fileData[i];
+            }
+
+            fileData = NULL;
+            delete[] fileData;
+            fileData = new int[subscript];
+
+            for (int i = 0; i < subscript; i++)
+            {
+                fileData[i] = tempData[i];
+            }
+
+            tempData = NULL;
+            delete[] tempData;
+            tempData = new int[subscript];
+
+            size = subscript;
+        }
+        fileData[subscript] = numbers;
+        subscript++;
+    }
+    for (int i = 0; i < subscript; i++)
+    {
+        std::cout << fileData[i] << " ";
+    }
+
+    std::system("pause");
+
+    inputStream.close();
+    delete[] fileData, tempData;
+}
